e1a042b7cc0a7855a864e3c4e5a8df719d3a61c3
yupa42 <89157073+yupa42@users.noreply.github.com>
2022-04-03 19:14:12 +0200

    opload fill

---
+++ js/hub.js
@@ -0,0 +1,104 @@
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+//get token in the cookie
+var token = getCookie("token");
+console.log(token);
+//get id user in the cookie
+var idUser = getCookie("idUser");
+console.log(idUser);
+//get type user in the cookie
+var typeUser = getCookie("typeUser");
+console.log(typeUser);
+//if the token is not null, display the hub page
+if (token == null) {
+    //window.location.href = "index.html";
+    }
+//if the typeUser is E then set typeUserLong to "eleves"
+if (typeUser == "E") {
+    var typeUserLong = "eleves";
+}
+//if the typeUser is P then set typeUserLong to "professeurs"
+if (typeUser == "P") {
+    var typeUserLong = "professeurs";
+}
+
+//if the token is invalid, display the login page
+fetch("https://api.ecoledirecte.com/v3/"+typeUser+"/"+idUser+"/timelineAccueilCommun.awp?verbe=get&v=4.6.0", {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
+    "content-type": "application/x-www-form-urlencoded",
+    "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"99\", \"Google Chrome\";v=\"99\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Linux\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "x-token": token
+  },
+  "referrer": "https://www.ecoledirecte.com/",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "data={}",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "omit"
+}).then(response => response.json())
+    .then(data => {
+        console.log(data);
+        //if the token is invalid, display the login page
+        if (data.message == "Token invalide !") {
+            //window.location.href = "index.html";
+            console.log("Token invalide !");
+        }
+        if (data.message == "Session expirée !") {
+            //window.location.href = "index.html";
+            console.log("Session expirée !");
+        }
+    }
+    );
+//get message not read in the api
+fetch("https://api.ecoledirecte.com/v3/"+typeUserLong+"/"+idUser+"/messages.awp?force=false&typeRecuperation=received&idClasseur=0&orderBy=date&order=desc&query=&onlyRead=&page=0&itemsPerPage=20&verbe=getall&v=4.6.0", {
+        "headers": {
+          "accept": "application/json, text/plain, */*",
+          "accept-language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
+          "content-type": "application/x-www-form-urlencoded",
+          "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"99\", \"Google Chrome\";v=\"99\"",
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": "\"Linux\"",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+          "x-token": token
+        },
+        "referrer": "https://www.ecoledirecte.com/",
+        "referrerPolicy": "strict-origin-when-cross-origin",
+        "body": "data={\n    \"anneeMessages\": \"2021-2022\"\n}",
+        "method": "POST",
+        "mode": "cors",
+        "credentials": "omit"
+      }).then(response => response.json())
+            .then(data => {
+                console.log(data);
+                //if data.data.pagination.messagesRecusNotReadCount > 0, set display message_nodif true and display data.data.pagination.messagesRecusNotReadCount
+                if (data.data.pagination.messagesRecusNotReadCount >= 1) {
+                    var message_nodif = document.getElementById("message_nodif");
+                    message_nodif.style.display = "block";
+                    message_nodif.innerHTML = data.data.pagination.messagesRecusNotReadCount;
+                }
+                console.log(data.data.pagination.messagesRecusNotReadCount);
+            }
+            );
+
---
+++ js/index.js
@@ -0,0 +1,48 @@
+//connect to ecoledirecte and the data to display them on my site
+function connectToEcoledirecte() {
+var login = document.getElementById("login").value;
+var password = document.getElementById("password").value;
+
+fetch("https://api.ecoledirecte.com/v3/login.awp?v=4.6.0", {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
+    "content-type": "application/x-www-form-urlencoded",
+    "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"99\", \"Google Chrome\";v=\"99\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Linux\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "x-token": ""
+  },
+  "referrer": "https://www.ecoledirecte.com/",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "data={\n    \"uuid\": \"\",\n    \"identifiant\": \""+login+"\",\n    \"motdepasse\": \""+password+"\",\n    \"isReLogin\": false\n}",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "omit"
+}).then(response => response.json())
+  .then(data => {
+    console.log(data);
+    //display the error if the login is not correct
+    if (data.message == "Identifiant et/ou mot de passe invalide !") {
+      document.getElementById("error").innerHTML = "Identifiant invalide !";
+      console.log("Identifiant invalide !");
+    }else if (data.message == "Mot de passe invalide !") {
+      document.getElementById("error").innerHTML = "Mot de passe invalide !";
+      console.log("Mot de passe invalide !");
+    } else if (data.message == "") {
+      //if the login is correct, save the token in the cookie
+      document.cookie = "token=" + data.token;
+      console.log("Token saved");
+      //get and save the idUser in the cookie in the data.account.[0].id
+      document.cookie = "idUser=" + data.data.accounts[0].id;
+      console.log("id saved")
+      //get and save the typeUser in the cookie in the data.account.[0].type
+      document.cookie = "typeUser=" + data.data.accounts[0].typeCompte;
+      console.log("typeCompte saved")
+      //switch to the hub page
+      window.location.href = "hub.html";
+    }
+  })};

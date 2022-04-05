a037c82b57fc6acc38ec02ca5634f4ac093423c0
yupa42 <89157073+yupa42@users.noreply.github.com>
2022-04-05 19:33:12 +0200

    Add files via upload

--- js/mesage.js
+++ js/mesage.js
@@ -69,3 +69,76 @@ fetch("https://api.ecoledirecte.com/v3/"+typeUser+"/"+idUser+"/timelineAccueilCo
         }
     }
     );
+    function mesage(){
+      un=document.getElementById("1");
+      deux=document.getElementById("2");
+      int=document.getElementById("int");
+      mes=document.getElementById("mes");
+      mes.style.display = "none";
+      int.style.display = "block";
+      un.style.width = "100%";
+      deux.style.width = "0%";
+      } 
+    function back(){
+      un=document.getElementById("1");
+      deux=document.getElementById("2");
+      int=document.getElementById("int");
+      mes=document.getElementById("mes");
+      un.style.width = "0%";
+      deux.style.width = "100%";
+      mes.style.display = "block";
+      int.style.display = "none";
+      } 
+  //get year scolar
+  var year = new Date().getFullYear();
+  //if actual date is in a 1 september > 30 december year = year "-" year + 1 else year = 1 - year "-" year  
+  if (new Date().getMonth() > 8) {
+    var yearScolar = year + "-" + (year + 1);
+  } else {
+    var yearScolar = (year - 1) + "-" + year;
+  }
+//get the mesage
+fetch("https://api.ecoledirecte.com/v3/"+typeUserLong+"/"+idUser+"/messages.awp?force=true&typeRecuperation=received&idClasseur=0&orderBy=date&order=desc&query=&onlyRead=&page=0&itemsPerPage=20&verbe=getall&v=4.6.0", {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
+    "content-type": "application/x-www-form-urlencoded",
+    "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"100\", \"Google Chrome\";v=\"100\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Linux\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "sec-gpc": "1",
+    "x-token": token
+  },
+  "referrer": "https://www.ecoledirecte.com/",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "data={\n    \"anneeMessages\": \""+yearScolar+"\"\n}",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "omit"
+});
+//convertir in js object
+var obj = JSON.parse(getmesage);
+console.log(obj);
+//create a card for each message
+for (var i = 0; i < obj.data.pagination.messagesRecusCount; i++) {
+    var card = document.createElement("div");
+    card.className = "card";
+    //card add random color
+    card.style.backgroundColor = randomColor({luminosity: 'light'});
+    //add onclick (mesage()) to card
+    card.onclick = "mesage()";
+    //add id to card
+    card.id = obj.data.messages.received[i].id;
+    var title = document.createElement("h2");
+    title.innerHTML = obj.data.messages.received[i].subject;
+    var content = document.createElement("p");
+    content.innerHTML = obj.data.messages.received[i].from.name;
+    card.appendChild(title);
+    card.appendChild(content);
+    document.getElementById("message_nodif_card").appendChild(card);
+    console.log(card);
+    
+}
